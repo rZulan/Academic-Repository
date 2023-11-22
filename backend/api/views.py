5378e7bd27fbc6df4b8f0a0aedac2e9bb146b836
Zulan <roi.zulan@gmail.com>
2023-11-22 18:14:13 +0800

    foosh

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from rest_framework_simplejwt.tokens import RefreshToken
 
 import jwt
 
@@ -22,8 +22,10 @@ class GoogleLoginView(APIView):
             serializer.is_valid(raise_exception=True)
 
             credential = serializer.validated_data['credential']
-            
-            access_token_payload = AccessToken().decode(credential, verify=False)
+            print(credential)
+
+            credential = 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjViMzcwNjk2MGUzZTYwMDI0YTI2NTVlNzhjZmE2M2Y4N2M5N2QzMDkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI2MjEzMzk1OTA5NDItMWVvOG9pNjB1MjJiOGluODRhY2w4ODkxbjQ3NXZuajYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI2MjEzMzk1OTA5NDItMWVvOG9pNjB1MjJiOGluODRhY2w4ODkxbjQ3NXZuajYuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDg0NDY3NTYzNjg4ODExMTE3MjQiLCJoZCI6ImRodnN1LmVkdS5waCIsImVtYWlsIjoiMjAyMDEwNjMxN0BkaHZzdS5lZHUucGgiLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwibmJmIjoxNzAwNjQ3MzQyLCJuYW1lIjoiQWxmcmVkbyBSb2kgTmFsdXoiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDMuZ29vZ2xldXNlcmNvbnRlbnQuY29tL2EvQUNnOG9jS3lDZS12cTZxamxybUlEX3NTLXhFOHNEYkVkRU0wV1I1aVFnQTljVW9PWUE9czk2LWMiLCJnaXZlbl9uYW1lIjoiQWxmcmVkbyBSb2kiLCJmYW1pbHlfbmFtZSI6Ik5hbHV6IiwibG9jYWxlIjoiZW4iLCJpYXQiOjE3MDA2NDc2NDIsImV4cCI6MTcwMDY1MTI0MiwianRpIjoiMGQ2M2UxNTBiMzk4ZjY3MzBiNjYxMThhOTQyYmVlZGQ0MjJhNGM0OCJ9.EbqNvd1wwfpzJblLJ6EI2dlSebSdwLKNIOiH10X4VHCILw0oKTUyGdc9jtOpwF5-IFHQv4MEDK2_GmtMhTPONlyQGx3pTmDuV5lZAEeNf7Qa_sXHBKo-PKNi-AGBGYgNWQWzRRJbPrSZwU22x81iTbxp37FIdpeL2l_mIBK0xfyXtmjcH1vwtOCg7wLiFL8cjxywatrWYdUoZQCY5HeXu_Ig2NYVsri4EKixeIN-ACoZZaOkab6sMDauhAxZrXjBcoOKc-uClqaebMAyyow9xVniaasf_PC_GXXFi9qg1Lzjfr2Dt8nbYsXz_Qp1dYXOG9GDg1Fr8HrsJWfkDP7_IQ'
+            access_token_payload = jwt.decode(credential, verify=False, algorithms=["RS256"])
             
             google_user_id = access_token_payload.get(access_token_payload.get('sub'))
             email = access_token_payload.get('email')
