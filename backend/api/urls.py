62718a7041af09d64d84f2445bd411178c9aac13
rZulan <roi.zulan@gmail.com>
2023-11-24 13:26:47 +0800

    documents list

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GoogleLoginView, UploadDocumentAPIView
+from .views import GoogleLoginView, UploadDocumentAPIView, DocumentsView
 
 urlpatterns = [
     path('login/', GoogleLoginView.as_view()),
     path('upload/', UploadDocumentAPIView.as_view(), name='upload_document'),
+    path('library/', DocumentsView.as_view(), name='upload_document'),
 ]
