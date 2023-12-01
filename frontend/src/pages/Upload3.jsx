d7e43151af44ddde73f52ca2a2f37be60237a372
whiskey00 <119928947+whiskey00@users.noreply.github.com>
2023-12-01 12:40:36 +0800

    up

--- frontend/src/pages/Upload3.jsx
+++ frontend/src/pages/Upload3.jsx
@@ -9,8 +9,8 @@ const Upload3 = () => {
     title: '',
     abstract: '',
     authors: '',
-    department: '', // Updated to use a dropdown
-    course: '',    // Updated to use a dropdown
+    department: '', 
+    course: '',
   });
 
   const handleFileChange = (e) => {
@@ -105,8 +105,9 @@ const Upload3 = () => {
     switch (step) {
       case 1:
         return (
-          <div className="container mx-auto my-10 p-6 bg-white rounded-lg shadow-lg w-[65%]">
+          <div className="container mx-auto my-10 p-6 bg-white rounded-lg shadow-lg w-[65%] border border-t-[7px] border-t-[#600414] ">
             <h1 className="text-2xl mb-4">Step 1</h1>
+            <h2 className='text-xl mb-4'>Information:</h2>
             <form>
               <div className="mb-4">
                 <label className="block text-gray-600">Title:</label>
@@ -255,14 +256,25 @@ const Upload3 = () => {
                 )}
               </select>
             </div>
+            
           </form>
-          <input type="file" id="fileinput" onChange={handleFileChange} />
-          <button
-            onClick={handleFileUpload}
-            className="bg-[#600414] text-white rounded-md px-4 py-2 transition duration-300 mt-4"
-          >
-            Upload
-          </button>
+    
+          <div className="mt-6 p-4 border border-gray-300 rounded-lg shadow-md">
+  <h2 className="text-xl mb-4">Upload Document:</h2>
+  <input type="file" id="fileinput" onChange={handleFileChange} className="mb-2" />
+  <button
+    onClick={handleFileUpload}
+    className="bg-[#600414] text-white rounded-md px-4 py-2 transition duration-300 mt-2"
+  >
+    Upload
+  </button>
+  <p className="my-4 italic text-gray-400">
+    Note: Uploading a document will automatically remove Figures/Images/Tables
+  </p>
+</div>
+
+
+
           <div className="flex justify-center mt-4">
             <button
               onClick={handleNextStep}
